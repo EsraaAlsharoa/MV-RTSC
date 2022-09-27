3fbd55a4027a370c187826f9ebbde65dc3a3e397
EsraaAlsharoa <95857663+EsraaAlsharoa@users.noreply.github.com>
2022-09-27 12:30:50 +0300

    Update MVRTSC.m

--- functions/MVRTSC.m
+++ functions/MVRTSC.m
@@ -23,7 +23,18 @@ function [Plabel,Timecost] = MVRTSC(A,mu,lambda,numClust)
 % Copyright (C)   <Esraa Al-Sharoa & Mohammad Al-Wardat>
 %  
 %%%%
-
+% This program is free software: you can redistribute it and/or modify
+% it under the terms of the GNU General Public License as published by
+% the Free Software Foundation.
+% 
+% This program is distributed in the hope that it will be useful,
+% but WITHOUT ANY WARRANTY; without even the implied warranty of
+% MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+% GNU General Public License for more details.
+% 
+% You should have received a copy of the GNU General Public License
+% along with this program.  If not, see <http://www.gnu.org/licenses/>.
+% 
 
 [n1,n2,n3]=size(A);
 I=eye(n1,n2);
